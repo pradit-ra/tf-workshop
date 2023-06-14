dec0fe9eff8038227541ede3c04b0d20a794576d
Pradit Rattanapongchinda <pradit.ra@tdshop.io>
2023-06-14 21:20:08 +0700

    AI-1057 Change to use gcs for state store

--- tf/versions.tf
+++ tf/versions.tf
@@ -6,4 +6,10 @@ terraform {
       version = ">=4.68.0"
     }
   }
+
+  backend "gcs" {
+    bucket                      = "tdshop-data-internal-tf-states"
+    prefix                      = "terraform/pradit"
+    impersonate_service_account = "terraform@tdshop-data-internal.iam.gserviceaccount.com"
+  }
 }
